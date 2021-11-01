e0cea489a3d1be379ecc466c4bdb4aafbd122e2b
taiiii123 <taiiii88uqtg@gmail.com>
2021-11-01 18:05:09 +0900

    add Sample01

---
+++ 変数/Sample01.java
@@ -0,0 +1,12 @@
+public class Sample01{
+    public static void main(String[] args){
+        
+        // 静的言語で型を宣言する必要がある。
+        String str = "String";
+        System.out.println(str);
+
+        // 文字列の代入
+        str = String.format("%s2", str);
+        System.out.println(str);
+    }
+}
