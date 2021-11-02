77c48ae10528f9c3183653fe7ea1c1d0b990a7d2
taiiii123 <taiiii88uqtg@gmail.com>
2021-11-02 20:57:59 +0900

    add Sample02

---
+++ 変数/Sample02.java
@@ -0,0 +1,31 @@
+public class Samople02{
+    public static void main(String[] args){
+        
+        // 真偽値
+        boolean a = true;
+        System.out.println(a);
+        
+        // char, int, float, long
+        char b = 'a';
+        int c = 1;
+        float d = 1.23F;
+        double e = 1.23e4;
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+
+        float f1 = Float.NaN;                  // 非数（Not a Number）
+        float f2 = Float.POSITIVE_INFINITY;    // 正の無限大
+        float f3 = Float.NEGATIVE_INFINITY;    // 負の無限大
+        double d1 = Double.NaN;                // 非数（Not a Number）
+        double d2 = Double.POSITIVE_INFINITY;  // 正の無限大
+        double d3 = Double.NEGATIVE_INFINITY;  // 負の無限大
+        System.out.println(f1);
+        System.out.println(f2);
+        System.out.println(f3);
+        System.out.println(d1);
+        System.out.println(d2);
+        System.out.println(d3);
+    }
+}
