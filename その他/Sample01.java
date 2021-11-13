9c2498cc3ae85bbdd2ac46b4ef76a5a2c73a6d6a
taiiii123 <taiiii88uqtg@gmail.com>
2021-11-13 17:42:40 +0900

    Sample01 少し修正

--- その他/Sample01.java
+++ その他/Sample01.java
@@ -2,9 +2,10 @@ class Sample01 {
     public static void main(String[] args) {
         
         // コマンド引数
-        int i = 1;
+        int i = 0;
         for(String s : args) {
             System.out.println("コマンドライン引数[" + i + "] = " + s);
+            i++;
         }
 
         for(i = 0; i < args.length; i++) {
