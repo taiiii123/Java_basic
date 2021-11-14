3c6ff9e756dc0125915ea3e24e30215b7eef0756
taiiii123 <taiiii88uqtg@gmail.com>
2021-11-14 21:40:20 +0900

    add Sample01

---
+++ クラス/Sample01.java
@@ -0,0 +1,8 @@
+import pack.SampleSub01;
+
+public class Sample01 {
+    public static void main(String[] args) {
+        SampleSub01 p = new SampleSub01();
+        p.print();
+    }
+}
---
+++ クラス/pack/SampleSub01.java
@@ -0,0 +1,7 @@
+package pack;
+
+public class SampleSub01 {
+    public void print() {
+        System.out.println("SampleSub01 has been accessed.");
+    }
+}
