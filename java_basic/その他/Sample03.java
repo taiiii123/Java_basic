e75d0f11445bcf4684c7efc245fa83cdc6880f52
taiiii123 <taiiii88uqtg@gmail.com>
2021-12-09 17:25:14 +0900

    add Sample03

--- java_basic/その他/Sample03.java
+++ java_basic/その他/Sample03.java
@@ -9,5 +9,18 @@ public class Sample03 {
         // 基本的な時刻表現
         LocalDateTime now = LocalDateTime.now();
         System.out.println(now);
+
+        // 時刻を指定して表示
+        LocalDateTime d = LocalDateTime.of(2021, 12, 9, 17, 21, 30);
+        System.out.println(d);
+
+        // int型に変換して表示
+        int year = d.getYear(); // 年
+        int month = d.getMonthValue(); // 月
+        int day = d.getDayOfMonth(); // 日
+        int hour = d.getHour(); // 時
+        int minute = d.getMinute(); // 分
+        int second = d.getSecond(); // 秒
+        System.out.println(year + "/" + month + "/" + day + " " + hour + ":" + minute + ":" + second);
     }
 }
