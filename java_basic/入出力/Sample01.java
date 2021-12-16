81a6e897ada1aa1279ab01ff0d2a58e532e5a266
taiiii123 <taiiii88uqtg@gmail.com>
2021-12-16 16:03:24 +0900

    add Sample01

---
+++ java_basic/入出力/Sample01.java
@@ -0,0 +1,25 @@
+/* ファイル入出力1 - 読み込み */
+/* 
+java.io.FileReaderを使用
+readメソッドで読み込み
+*/
+import java.io.*;
+
+
+public class Sample01 {
+    public static void main(String args[]) {
+        try {
+            FileReader fr = new FileReader("data.txt"); // ファイルを読み込む
+            int input = fr.read();
+
+            // ファイルを読み込んだら、ファイルを閉じる
+            while (input != -1) { // -1はEOF
+                System.out.print((char)input);
+                input = fr.read();
+            }
+            fr.close();
+        } catch(Exception e) {
+            System.out.println("ファイルが見つかりません");
+        } 
+    }
+}
