b2f69bed3385f68efffd9ea778f66fe8611ac939
taiiii123 <taiiii88uqtg@gmail.com>
2021-12-17 19:52:40 +0900

    add Sample04

--- java_basic/入出力/Sample04.java
+++ java_basic/入出力/Sample04.java
@@ -1,17 +1,18 @@
-/* ファイル入出力3 - 書き込み(上書き) */
+/* ファイル入出力4 - 書き込み(追加) */
 /* 
 java.io.FileWriteを使用
 FileWriterクラスのwriteメソッドで書き込み
 1文字ずつ書き込む
+FileWriter("ファイル名", true)で追加書き込み
 */
 import java.io.*;
 
 
-public class Sample03 {
+public class Sample04 {
     public static void main(String[] args) {
         try {
-            FileWriter fw = new FileWriter("data2.txt"); // 新規作成(上書き)
-            fw.write("こんにちは"); // 文字列を書き込む
+            FileWriter fw = new FileWriter("data2.txt", true); // 新規作成(上書き)
+            fw.write("\r\nHello"); // 文字列を書き込む
             fw.close();
         } catch(Exception e) {
             System.out.println("ファイルが見つかりません");
